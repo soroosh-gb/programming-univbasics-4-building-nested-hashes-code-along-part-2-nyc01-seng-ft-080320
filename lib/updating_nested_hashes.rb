6a7d8f48a3c2ad47c500b8ff32c220d535839566
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-29 22:02:15 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,8 +21,8 @@ def update_status
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  epic_tragedy[:hero][:status] = "dead"
-  epic_tragedy[:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
 
